08f1de9c7247d180eaf4ccc9270fcd693acd3247
dark <929609968@qq.com>
2018-02-27 00:23:36 +0800

    upload

---
+++ WebRoot/index.jsp
@@ -0,0 +1,66 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8" isELIgnored="false"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%
+	String path = request.getContextPath();
+	String basePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort()
+			+ path + "/";
+%>
+
+<html>
+<head>
+<base href="<%=basePath%>">
+
+<title>九宫格日记网</title>
+
+<link href="css/base.css" rel="stylesheet">
+<link href="css/index.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/lightbox.min.css">
+
+<!--[if lt IE 9]>
+<script src="js/modernizr.js"></script>
+<![endif]-->
+<script src="js/scrollReveal.js"></script>
+</head>
+
+<body>
+	<jsp:include page="header.jsp"></jsp:include>
+
+		<script type="text/javascript">
+			var h = window.location.href;
+			if (h.substring(h.lastIndexOf('/')+1)=="index.jsp" || h.substring(h.lastIndexOf('/')+1)=="")
+				window.location="Diary?Page=1&from=index";
+		</script>
+
+	<article>
+		<div class="container">
+			<ul class="cbp_tmtimeline">
+				<c:forEach items="${requestScope.diarys }" var="d">
+					<li><time class="cbp_tmtime">
+							<span>${d.timeBeg }</span> <span>${d.timeEnd }</span>
+						</time>
+						<div class="cbp_tmicon"></div>
+						<div class="cbp_tmlabel" data-scroll-reveal="enter right over 0.5s">
+							
+							<p>
+								<span class="blogpic"><a href="${d.address }"
+									data-lightbox="image-1"><img src="${d.address }"> </a> </span>
+									<div style="font-size:1.2em;line-height:2em;">
+										<h2>${d.title }</h2>
+										作者：${d.username} <br>
+										日期：${d.timeEnd } <br>
+										时间：${d.timeBeg }
+									</div>
+							</p>
+							<!-- <a href="${d.id }" target="_blank" class="readmore">阅读全文&gt;&gt;</a> -->
+						</div></li>
+				</c:forEach>
+			</ul>
+			<div class="page">
+				${requestScope.pageinfo }
+			</div>
+		</div>
+	</article>
+
+	<jsp:include page="footer.jsp"></jsp:include>
+</body>
+</html>
---
+++ src/servlet/DiarySave.java
@@ -0,0 +1,79 @@
+package servlet;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mysql.fabric.Server;
+
+import dao.DiaryDAO;
+import sun.misc.BASE64Decoder;
+
+public class DiarySave extends HttpServlet {
+	private static final long serialVersionUID = -5815196502656815479L;
+
+	public DiarySave() {
+		super();
+	}
+
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String title = request.getParameter("title");
+		int userid = (int) request.getSession().getAttribute("id");
+		String username = (String) request.getSession().getAttribute("username");
+		new DiaryDAO().InsertDiary(title, userid, username);
+		int articleid = new DiaryDAO().SelectDiary(userid).get(0).getId();
+		GenerateImage(request.getParameter("pic"), String.valueOf(articleid),
+				getServletConfig().getServletContext().getRealPath("/") + "images");
+		response.sendRedirect("Diary?Page=1&from=mine");
+	}
+
+	public static boolean GenerateImage(String imgStr, String imgName, String imgPath) {
+		// 对Base64字符串解码并生成图片
+		if (imgStr == null) {
+			return false;
+		} // 图像数据为空
+		imgStr = imgStr.substring(22);
+		BASE64Decoder decoder = new BASE64Decoder();
+		try {
+			// Base64解码
+			byte[] b = decoder.decodeBuffer(imgStr);
+			/*
+			 * for (int i = 0; i < b.length; ++i) { if (b[i] < 0) { b[i] += 256;
+			 * } // 调整异常数据 }
+			 */
+			File headPath = new File(imgPath);// 获取文件夹路径
+			if (!headPath.exists()) {// 判断文件夹是否创建，没有创建则创建新文件夹
+				headPath.mkdirs();
+			}
+			// 定义图片路径
+			String imgFilePath = imgPath + "/" + imgName + ".jpg";
+			// 新生成的图片
+			OutputStream out = new FileOutputStream(imgFilePath);
+			out.write(b);
+			out.flush();
+			out.close();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public void init() throws ServletException {
+	}
+
+}
